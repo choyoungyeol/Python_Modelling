258a79737b6ace83dae484947a902bf62cc7805a
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2023-07-12 15:57:00 +0900

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2
 from datetime import datetime
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, TimeDistributed, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 
 # Tipburn 데이터 폴더 경로
@@ -19,7 +20,6 @@ for filename in os.listdir(tipburn_folder):
         img = cv2.imread(os.path.join(tipburn_folder, filename))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 그레이스케일로 변환
         img = cv2.resize(img, (32, 32))  # 이미지 크기 조정
-        img = np.expand_dims(img, axis=2)  # 차원 확장
         tipburn_images.append(img)
 
         # 파일명에서 날짜 정보 추출
@@ -50,7 +50,6 @@ for filename in os.listdir(normal_folder):
         img = cv2.imread(os.path.join(normal_folder, filename))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 그레이스케일로 변환
         img = cv2.resize(img, (32, 32))  # 이미지 크기 조정
-        img = np.expand_dims(img, axis=2)  # 차원 확장
         normal_images.append(img)
 
         # 파일명에서 날짜 정보 추출
@@ -86,13 +85,33 @@ y = y[sorted_indices]
 # 데이터 분할: 훈련 데이터와 테스트 데이터로 나눔
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# # 데이터 reshape
+# X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
+# X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
+#
+# # y_train과 y_test를 2차원으로 변환
+# # 주석으로 표시한 위치에 아래 코드 추가
+# # -------------------------------------
+# y_train = np.reshape(y_train, (-1, 1))
+# y_test = np.reshape(y_test, (-1, 1))
+# # -------------------------------------
+
+print("X_train의 차원:", X_train.ndim)
+print("X_test의 차원:", X_test.ndim)
+print("X_train의 모양:", X_train.shape)
+print("X_test의 모양:", X_test.shape)
+
 # 데이터 reshape
-X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2])
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2])
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1)
 
 # LSTM 모델 구축
 model = Sequential()
-model.add(LSTM(64, input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(X_train.shape[1], X_train.shape[2], 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(RepeatVector(1))
+model.add(LSTM(64))
 model.add(Dense(1, activation='sigmoid'))
 
 # 모델 컴파일
@@ -112,7 +131,6 @@ loaded_model = load_model("D:/AI/tipburn_lstm_model.h5")
 # 모델 평가
 loss, accuracy = loaded_model.evaluate(X_test, y_test)
 print("평가 결과 - 손실: {:.4f}, 정확도: {:.4f}".format(loss, accuracy))
-
 
 import cv2
 import numpy as np
@@ -145,11 +163,9 @@ predicted_timestamps = timestamps[sorted_indices][-len(X_test):]
 predicted_tipburn_timestamps = predicted_timestamps[predictions.flatten() > 0.5]
 
 # 시퀀스 예측
-input_sequence = np.expand_dims(X_test, axis=1)  # 시퀀스 입력 형태로 변환
+input_sequence = np.expand_dims(X_test, axis=-1)  # 시퀀스 입력 형태로 변환
 sequence_predictions = loaded_model.predict(input_sequence)
 predicted_timestamp = timestamps[np.argmax(sequence_predictions)]  # 최댓값을 갖는 타임스탬프 선택
 
 print("Tipburn 예측 결과: {} (확률: {:.2f}%)".format(prediction_label, prediction_confidence * 100))
 print("Tipburn 발생 시점 예측: {} 시점".format(predicted_timestamp))
-
-
